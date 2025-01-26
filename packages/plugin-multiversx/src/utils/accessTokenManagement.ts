86f2af9b484ea17cff3c954162df128977e54d5c
elpulpo0 <chris.elpulpo@gmail.com>
2025-01-26 23:46:11 +0100

    update function isUserAuthorized

--- packages/plugin-multiversx/src/utils/accessTokenManagement.ts
+++ packages/plugin-multiversx/src/utils/accessTokenManagement.ts
@@ -5,7 +5,10 @@ export function isUserAuthorized(
     runtime: IAgentRuntime
 ): boolean {
     const authorizedUserId = runtime.getSetting("ACCESS_TOKEN_MANAGMENT_TO");
-    console.log("UserID from message:", userId); // Log de l'ID de l'utilisateur
-    console.log("Authorized UserID:", authorizedUserId); // Log de l'ID autorisé
-    return userId === authorizedUserId; // Compare l'ID de l'utilisateur avec l'ID autorisé
+    console.log("UserID from message:", userId);
+    console.log("Authorized UserID:", authorizedUserId);
+    if (authorizedUserId === "everyone") {
+        return true;
+    }
+    return userId === authorizedUserId;
 }
